db62af4ec51d454caeb8545a63153cf52fa043de
YohanaDes12 <butarbutaryohana@gmail.com>
2025-09-22 21:00:08 +0700

    Tugas 2 Pertemuan 3

---
+++ app/src/main/java/com/unsoed/informatikamobile/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.unsoed.informatikamobile // Ganti dengan nama package Anda
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.unsoed.informatikamobile.databinding.ActivityMainBinding // Ganti dengan nama package Anda
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        binding.btnToPage2.setOnClickListener {
+            startActivity(Intent(this, Halaman2Activity::class.java))
+        }
+    }
+}
